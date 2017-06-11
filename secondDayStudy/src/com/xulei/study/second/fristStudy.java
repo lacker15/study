14060428c20a8dfec27664272c4eb8d42a4bdd91
xulei <123@163.com>
2017-06-11 12:56:41 +0800

    第一天

---
+++ secondDayStudy/src/com/xulei/study/second/fristStudy.java
@@ -0,0 +1,22 @@
+package com.xulei.study.second;
+
+/**
+ * Created by Administrator on 2017/6/11.
+ * 第一天学习乘法表
+ */
+public class fristStudy {
+    public static void main(String[] args) {
+        outPutTable(12);
+    }
+    //输出九九乘法表
+    private static void outPutTable(Integer num){
+        //2个循环表示出表
+        for(int i = 1; i <= num; i++){
+            //第二个循环
+            for(int j = 1; j <= i; j++){
+                System.out.print(i + "*" + j + "=" + i * j+ "\t");
+            }
+            System.out.println();
+        }
+    }
+}
